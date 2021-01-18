9581d682c5062653e98a877810361904c0bf76d1
jep-mikecurtis <mcurtis@jepcapital.com>
2021-01-17 18:05:28 -0800

    Nav padding fix on the layout

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -40,10 +40,14 @@ const Login = () => {
 
     return (
         <div className="container mx-auto flex justify-center py-20">
-            <Card imgSrc="https://images.unsplash.com/photo-1517817748493-49ec54a32465?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80">
+            <Card imgSrc="https://images.unsplash.com/photo-1557116544-1fb2be0d8e00?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80">
                 <h1 className="text-white font-bold flex-auto text-xl font-semibold">
                     Login
                 </h1>
+
+                <p className="text-pink-600">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque possimus totam et voluptate nesciunt, quae ab quisquam esse maiores pariatur?
+                </p>
 
                 {/* Register FORM */}
                 <div className="flex flex-col space-y-2 mt-4">
